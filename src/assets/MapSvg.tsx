a14552a510030c2d569dd8465f06b5a1386bae1a
GolfGrab <surapus01@gmail.com>
2023-01-23 15:37:56 +0700

    🍱 Add Svg assets

---
+++ src/assets/MapSvg.tsx
@@ -0,0 +1,294 @@
+const MapSvg = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      version="1.1"
+      id="Layer_1"
+      x="0px"
+      y="0px"
+      viewBox="0 0 255 100"
+      enableBackground="new 0 0 255 100"
+      xmlSpace="preserve"
+    >
+      <link type="text/css" rel="stylesheet" id="dark-mode-custom-link" />
+      <link type="text/css" rel="stylesheet" id="dark-mode-general-link" />
+      <style
+        lang="en"
+        type="text/css"
+        id="dark-mode-custom-style"
+        dangerouslySetInnerHTML={{ __html: "" }}
+      />
+      <style
+        lang="en"
+        type="text/css"
+        id="dark-mode-native-style"
+        dangerouslySetInnerHTML={{ __html: "" }}
+      />
+      <style
+        lang="en"
+        type="text/css"
+        id="dark-mode-native-sheet"
+        dangerouslySetInnerHTML={{ __html: "" }}
+      />
+      <g>
+        <rect
+          x="-78.176"
+          y="-180.025"
+          transform="matrix(0.7071 0.7071 -0.7071 0.7071 78.7514 -88.307)"
+          fill="#F3F4F4"
+          width="448.295"
+          height="461.865"
+        />
+        <rect
+          x="68.092"
+          y="-14.324"
+          transform="matrix(0.7071 0.7071 -0.7071 0.7071 30.1288 -53.798)"
+          fill="#83CDAC"
+          width="23.824"
+          height="47.587"
+        />
+        <rect
+          x="189.235"
+          y="-32.404"
+          transform="matrix(0.7071 0.7071 -0.7071 0.7071 67.8495 -148.7645)"
+          fill="#83CDAC"
+          width="48.528"
+          height="79.846"
+        />
+        <rect
+          x="20.892"
+          y="69.63"
+          transform="matrix(0.7071 0.7071 -0.7071 0.7071 85.8252 -2.9809)"
+          fill="#8AD0FF"
+          width="51.239"
+          height="64.959"
+        />
+        <rect
+          x="244.957"
+          y="-19.4"
+          transform="matrix(0.7071 0.7071 -0.7071 0.7071 88.9665 -180.4892)"
+          fill="#8AD0FF"
+          width="34.792"
+          height="73.095"
+        />
+        <rect
+          x="160.835"
+          y="9.648"
+          transform="matrix(0.7071 -0.7071 0.7071 0.7071 -59.711 158.8013)"
+          fill="#ECECEC"
+          width={2}
+          height="283.661"
+        />
+        <rect
+          x="185.132"
+          y="18.46"
+          transform="matrix(0.7071 -0.7071 0.7071 0.7071 11.8483 149.2889)"
+          fill="#ECECEC"
+          width={2}
+          height="83.765"
+        />
+        <rect
+          x="269.683"
+          y="-297.99"
+          transform="matrix(0.7071 -0.7071 0.7071 0.7071 131.4838 169.7787)"
+          fill="#ECECEC"
+          width={2}
+          height="448.33"
+        />
+        <rect
+          x="-33.303"
+          y="-54.153"
+          transform="matrix(0.7071 -0.7071 0.7071 0.7071 -28.7033 -14.8713)"
+          fill="#ECECEC"
+          width={2}
+          height="162.73"
+        />
+        <rect
+          x="-14.895"
+          y="153.285"
+          transform="matrix(0.7071 -0.7071 0.7071 0.7071 -69.3363 141.1781)"
+          fill="#ECECEC"
+          width="301.287"
+          height={2}
+        />
+        <rect
+          x="108.731"
+          y="135.842"
+          transform="matrix(0.7071 -0.7071 0.7071 0.7071 -10.3491 248.6981)"
+          fill="#ECECEC"
+          width="372.598"
+          height={2}
+        />
+        <rect
+          x="16.844"
+          y="151.759"
+          transform="matrix(0.7071 -0.7071 0.7071 0.7071 -35.4387 219.9615)"
+          fill="#ECECEC"
+          width="461.907"
+          height={2}
+        />
+        <rect
+          x="213.493"
+          y="-16.408"
+          transform="matrix(0.7071 -0.7071 0.7071 0.7071 57.7853 153.7561)"
+          fill="#75BA98"
+          width={2}
+          height="47.065"
+        />
+        <rect
+          x="16.648"
+          y="-109.763"
+          transform="matrix(0.7071 -0.7071 0.7071 0.7071 34.9086 0.1602)"
+          fill="#ECECEC"
+          width={2}
+          height="135.409"
+        />
+        <rect
+          x="-39.619"
+          y="-59.165"
+          transform="matrix(0.7071 -0.7071 0.7071 0.7071 -7.6729 -28.8149)"
+          fill="#ECECEC"
+          width={2}
+          height="108.039"
+        />
+        <rect
+          x="-147.751"
+          y="-14.33"
+          transform="matrix(0.7071 -0.7071 0.7071 0.7071 32.7425 55.3864)"
+          fill="#FFFFFF"
+          width="461.959"
+          height="4.999"
+        />
+        <rect
+          x="-55.991"
+          y="77.416"
+          transform="matrix(0.7071 -0.7071 0.7071 0.7071 -5.2576 147.1383)"
+          fill="#FFFFFF"
+          width="461.947"
+          height="4.999"
+        />
+        <rect
+          x="-42.017"
+          y="132.912"
+          transform="matrix(0.7071 -0.7071 0.7071 0.7071 -67.6466 104.5112)"
+          fill="#ECECEC"
+          width="268.7"
+          height={2}
+        />
+        <rect
+          x="-167.137"
+          y="-32.207"
+          transform="matrix(0.7071 -0.7071 0.7071 0.7071 40.7667 36.006)"
+          fill="#ECECEC"
+          width="461.967"
+          height={2}
+        />
+        <rect
+          x="69.22"
+          y="60.812"
+          transform="matrix(0.7071 -0.7071 0.7071 0.7071 -14.3601 88.9548)"
+          fill="#ECECEC"
+          width="61.956"
+          height={2}
+        />
+        <rect
+          x="-29.778"
+          y="103.609"
+          transform="matrix(0.7071 -0.7071 0.7071 0.7071 -16.1056 173.3353)"
+          fill="#FFFFFF"
+          width="461.918"
+          height="4.999"
+        />
+        <rect
+          x="205.168"
+          y="-2.34"
+          transform="matrix(0.7071 -0.7071 0.7071 0.7071 -27.8972 183.5929)"
+          fill="#FFFFFF"
+          width="4.999"
+          height="255.622"
+        />
+        <path
+          fill="#FEE379"
+          d="M92.259,49.378c-9.793,0-17.894-3.996-22.398-7.789l-0.252-0.231L-45.961-74.213l5.657-5.656L75.123,35.558   c1.761,1.401,20.604,15.461,42.108-6.039l167-167.001l5.657,5.656l-167,167.001C112.243,45.82,101.498,49.378,92.259,49.378z"
+        />
+        <rect
+          x="-128.353"
+          y="3.567"
+          transform="matrix(0.707 -0.7072 0.7072 0.707 24.7136 74.7907)"
+          fill="#FEE379"
+          width="461.959"
+          height={8}
+        />
+        <rect
+          x="-204.245"
+          y="-72.325"
+          transform="matrix(0.707 -0.7072 0.7072 0.707 56.1491 -1.1106)"
+          fill="#FEE379"
+          width="461.959"
+          height={8}
+        />
+        <rect
+          x="-7.345"
+          y="124.575"
+          transform="matrix(0.707 -0.7072 0.7072 0.707 -25.4094 195.8132)"
+          fill="#FEE379"
+          width="461.959"
+          height={8}
+        />
+        <rect
+          x="-90.098"
+          y="43.323"
+          transform="matrix(0.7071 -0.7071 0.7071 0.7071 8.8618 113.0396)"
+          fill="#FFFFFF"
+          width="461.959"
+          height="4.999"
+        />
+        <rect
+          x="89.034"
+          y="-120.354"
+          transform="matrix(0.7071 -0.7071 0.7071 0.7071 -46.1593 96.1916)"
+          fill="#FEE379"
+          width={8}
+          height="448.338"
+        />
+        <rect
+          x="168.634"
+          y="-199.954"
+          transform="matrix(0.7071 -0.7071 0.7071 0.7071 33.4409 129.1632)"
+          fill="#FEE379"
+          width={8}
+          height="448.338"
+        />
+        <rect
+          x="245.699"
+          y="-277.018"
+          transform="matrix(0.7071 -0.7071 0.7071 0.7071 110.5052 161.0843)"
+          fill="#FEE379"
+          width={8}
+          height="448.338"
+        />
+        <rect
+          x="41.386"
+          y="-72.706"
+          transform="matrix(0.7071 -0.7071 0.7071 0.7071 -93.8072 76.4552)"
+          fill="#FEE379"
+          width={8}
+          height="448.338"
+        />
+        <circle fill="#FEE379" cx="71.99" cy="38.203" r="13.053" />
+        <circle fill="#FFFFFF" cx="71.99" cy="38.203" r="4.473" />
+        <rect
+          x="287.345"
+          y="4.03"
+          transform="matrix(0.7071 -0.7071 0.7071 0.7071 17.6033 232.8247)"
+          fill="#FFFFFF"
+          width="4.999"
+          height="182.267"
+        />
+      </g>
+    </svg>
+  );
+};
+
+export default MapSvg;
